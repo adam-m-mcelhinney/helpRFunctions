411c1c47d78ea05271ed2b2fa600e6aaea66e58b
amcelhinney <amcelhinney@enova.com>
2014-11-05 14:22:55 -0600

    Removed duplicate

--- R/parse.email.R
+++ R/parse.email.R
@@ -1,19 +1,17 @@
-#' Splits an email address, or vector of email address, of the form local-part@ domain, into the local and domain pieces.
-#' 
+#' Splits an email address, or vector of email address, of the form local-part@@domain, into the local and domain pieces.
 #' 
 #' @param emails An email address, or vector of email addresses.
 #' @return A list containing the elements local and domain.
 #' @examples 
 #' 
-#' emails = c('test323@gmail.com', 'not an email address', 'test2@email.org')
+#' emails = c('test323@@gmail.com', 'not an email address', 'test2@@email.org')
 #' e = parse.email(emails)
 #' e$local
 #' e$domain
-#' e = parse.email('test323@gmail.com')
+#' e = parse.email('test323@@gmail.com')
 #' e$local
 #' e$domain
 #' 
-
 
 parse.email <- function(emails) {
 	email.list <- strsplit(emails, split = '@')
