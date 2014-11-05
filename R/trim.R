17f1d03e15af1063019bcd1b8853f63d69267ad3
amcelhinney <amcelhinney@enova.com>
2014-11-05 11:28:47 -0600

    Added function to trim leading, trailing or both types of whitespace from a string, or vector of strings.

---
+++ R/trim.R
@@ -0,0 +1,31 @@
+#' Trims whitespace from each element in a chacter vector.
+#' Adapted from this post on \href{http://stackoverflow.com/questions/2261079/how-to-trim-leading-and-trailing-whitespace-in-r}{StackOverflow}
+#' 
+#' @param x A character vector containing the 
+#' @param white.space Optional. Acceptable values are "leading", "trailing", or "both". Defaults to both. Do you want to trim the "leading" white space, the "trailing" white space, or "both" the leading and trailing white space.
+#' @return A vector containing the bucketized values.
+#' @examples 
+#' 
+#'x <-c(' leading whitespace', 'trailing whitespace ', ' both leading and trailing white space ')
+#'trim(x)
+#'trim(x, 'leading')
+#'trim(x, 'trailing')
+#'trim(x, 'other') # throws error
+
+trim <- function(x, white.space = 'both'){
+	if(!(white.space  %in% c('leading', 'trailing', 'both'))) {
+		stop(paste(white.space, ' is not an acceptable value for white.space.'))
+		}
+	if(white.space == 'both'){
+		trim.str <- "^\\s+|\\s+$"
+	} else if (white.space == 'leading'){
+		trim.str <- "^\\s+"
+	} else {
+		trim.str = "\\s+$"
+	}
+	return (gsub(trim.str, "", x))
+}
+
+
+
+
