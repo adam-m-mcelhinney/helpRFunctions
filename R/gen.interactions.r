461cf8614aa6675f31f1ec9d62320786367609fe
amcelhinney <amcelhinney@enova.com>
2014-11-05 17:26:28 -0600

    Fixed error with the generate interactions function.

--- R/gen.interactions.r
+++ R/gen.interactions.r
@@ -5,12 +5,17 @@
 #' @param return.all Optional. Defaults to FALSE. If true, the functon returns all of the columns in the data frame, including the interactions. If false, the function only returns the interaction columns.
 #' @return A new data frame with either all of the columns, or just the new interaction columns (depending on vale of return.all parameter).
 #' @examples 
+#' df <- data.frame(matrix(rnorm(100), nrow = 10))
+#' df <- cbind(df, rep('A', nrow(df)))
+#' df[1,1] <- NA
+#' gen.interaction(df)
+#' gen.interaction(df, interaction.vars = c("X1", "X2"))
 
-df <- data.frame(matrix(rnorm(100), nrow = 10))
-df <- cbind(df, rep('A', nrow(df)))
-gen.interaction(df)
 
 gen.interaction <- function(df, interaction.vars = names(df), return.all = FALSE) {
+	
+	orig <- df # Save the original data frame somewhere
+	df <- df[,interaction.vars] # Only get the columns we are interested in
 	
 	n <- nrow(df)
 	# Check is the variables are numeric. If not, ignore them.
@@ -25,13 +30,15 @@ gen.interaction <- function(df, interaction.vars = names(df), return.all = FALSE
 			}	
 	}
 	
-	m <- length(int.vars)
+	df <- df[,int.vars] # Get rid of the variables determined to be non-numeric
+	w = length(int.vars)
+	m <- sum(1:w)
 	# Initialize the data frame for the required size.
-	int.df <- data.frame(matrix(rep(NA, n * n * m), nrow = n))
-	int.names <- c(rep('', m * m))
+	int.df <- data.frame(matrix(rep(NA, n * m), nrow = n))
+	int.names <- c(rep('', m))
 	z = 1 # Counter
-	for(i in 1:j){
-		for(q in 1:j) {
+	for(i in 1:w){
+		for(q in i:w) {
 			int.names[z] <- paste0(int.vars[i],'.', int.vars[q])
 			int.df[,z] <- df[,i] * df[,q]
 			z <- z + 1
@@ -42,7 +49,7 @@ gen.interaction <- function(df, interaction.vars = names(df), return.all = FALSE
 	if(return.all == FALSE) {
 		return(int.df)
 	} else {
-		return(cbind(df, int.df))
+		return(cbind(orig, int.df))
 	}
 	
 	
