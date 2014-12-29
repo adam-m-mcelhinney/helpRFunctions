bbb755687ea3532b9947b9d6834b2343d1cfae71
Nan-Xia <nxia@enova.com>
2014-12-29 13:53:14 -0600

    Update amortization_function.R

--- R/amortization_function.R
+++ R/amortization_function.R
@@ -13,8 +13,9 @@ library(nleqslv)
 #'interest_rate=c(0.4,0.3)
 #'funding_date=c(1/1/2014, 1/5/2014)
 #'last_due_date=c(6/1/2014, 1/5/2015)
-#'installment_due_dates[1,]=c('1/1/2014','2/1/2014','3/15/2014','5/1/2014, '6/1/2014')
-#'installment_due_dates[2,]=c('1/5/2014','3/12/2014','6/2/2014','9/1/2014, '1/5/2015')
+#'installment_due_dates=matrix(nrow=2, ncol=5)
+#'installment_due_dates[1,]=c('1/1/2014','2/1/2014','3/15/2014','5/1/2014', '6/1/2014')
+#'installment_due_dates[2,]=c('1/5/2014','3/12/2014','6/2/2014','9/1/2014', '1/5/2015')
 #'amortize(principal, interest_rate, funding_date, last_due_date, installment_due_dates)
 
 
